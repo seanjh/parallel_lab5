50e915d79c34fdf6ac99d62793863efbd8fd49b8
Sean Herman <seanherman@gmail.com>
2015-04-25 23:04:44 -0400

    initial commit with python utils

---
+++ utils/ppm_convert.py
@@ -0,0 +1,145 @@
+import argparse
+import os
+import calendar
+import subprocess
+from os.path import isfile
+from datetime import datetime
+
+
+FILTERED_OUTPUT_DIR = 'filtered'
+TIMESTAMP = str(calendar.timegm(datetime.now().timetuple()))
+
+
+def extension_matches(filename, matches):
+    parts = filename.split('.')
+    extension = parts.pop().lower()
+    return extension in matches
+
+
+def is_jpeg(filename):
+    return extension_matches(filename, ['jpg', 'jpeg'])
+
+
+def is_png(filename):
+    return extension_matches(filename, ['png'])
+
+
+def is_tiff(filename):
+    return extension_matches(filename, ['tif', 'tiff'])
+
+
+def is_gif(filename):
+    return extension_matches(filename, ['gif'])
+
+
+def is_ppm(filename):
+    return extension_matches(filename, ['ppm'])
+
+
+def convert_to_ppm(command, infile, outfile):
+    print 'netbpm command: %s, infile: %s, outfile: %s' % (command, infile, outfile)
+    with open(outfile, 'wb') as fh:
+        command_args = [command, infile]
+        ppm_p = subprocess.Popen(command_args, stdout=subprocess.PIPE)
+        # plain_p = subprocess.Popen(['pnmtoplainpnm'], stdin=ppm_p.stdout, stdout=outfile)
+        plain_p = subprocess.Popen(['pnmtopnm', '-plain'], stdin=ppm_p.stdout, stdout=fh)
+        plain_p.wait()
+        # output = subprocess.check_output(['pnmtoplainpnm'], stdout=outfile)
+        # command_args = [command, infile]
+        # out, err = subprocess.check_output(command_args)
+        # outfile.write(out)
+        # p = Popen(command_args, )
+        # subprocess.call(command_args)
+
+
+def convert_to_original(command, infile, outfile):
+    print 'netbpm command: %s, infile: %s, outfile: %s' % (command, infile, outfile)
+    with open(outfile, 'wb') as fh:
+        command_args = [command, infile]
+        ppm_p = subprocess.Popen(command_args, stdout=fh)
+        ppm_p.wait()
+        # orig_p = subprocess.Popen([], stdin=ppm_p.stdout, stdout=fh)
+        # orig_p.wait()
+
+
+def output_pnm_filename(diretory, input_file, old_extension, new_extension):
+    print 'directory %s, input %s, extension %s' % (diretory, input_file, new_extension)
+    parts = input_file.split('.')
+    parts.pop()
+    filename = ''.join([''.join(parts), '~~', old_extension, '~~', '.', new_extension])
+    return os.path.abspath(os.path.join(diretory, filename))
+
+
+def output_original_filename(directory, filename):
+    # stuff~~jpg~~.ppm --> [stuff, jpg, .ppm]
+    # print 'FILENAME: %s' % filename
+    parts = filename.split('~~')
+    # print 'PARTS: %s' % parts
+    ppm_ext = parts.pop()
+    # print 'Ditching %s ' % parts.pop()  # ppm extension
+    original_extension = parts.pop()
+    # print 'Original extension %s' % original_extension
+    parts.insert(0, ''.join([TIMESTAMP, '-']))
+    root_filename = ''.join(parts)
+    full_filname = ''.join([root_filename, '.', original_extension])
+    return os.path.join(directory, FILTERED_OUTPUT_DIR, full_filname)
+
+
+def files_to_original(directory):
+    filtered_directory = os.path.join(directory, FILTERED_OUTPUT_DIR)
+    if not os.path.exists(filtered_directory):
+        print 'Making directory for filtered files: %s' % os.path.abspath(filtered_directory)
+        os.mkdir(filtered_directory)
+
+    for file_ in os.listdir(directory):
+        filename = os.path.join(directory, file_)
+        if is_ppm(file_):
+            out_filename = output_original_filename(directory, file_)
+            print 'NEW FILENAME: %s' % out_filename
+            if is_jpeg(out_filename):
+                convert_to_original('pnmtojpeg', filename, out_filename)
+            elif is_png(filename):
+                convert_to_original('pnmtopng', filename, out_filename)
+            elif is_tiff(filename):
+                convert_to_original('pamtotiff', filename, out_filename)
+            elif is_gif(filename):
+                convert_to_original('pamtogif', filename, out_filename)
+        else:
+            pass
+            # print 'Skipping non-PPM file %s' % file_
+
+
+def files_to_ppm(directory):
+    for file_ in os.listdir(directory):
+        filename = os.path.join(directory, file_)
+        if isfile(filename):
+            if is_jpeg(filename):
+                out_filename = output_pnm_filename(directory, file_, 'jpg', 'ppm')
+                convert_to_ppm('jpegtopnm', filename, out_filename)
+            elif is_png(filename):
+                out_filename = output_pnm_filename(directory, file_, 'png', 'ppm')
+                convert_to_ppm('pngtopnm', filename, out_filename)
+            elif is_tiff(filename):
+                out_filename = output_pnm_filename(directory, file_, 'tif', 'ppm')
+                convert_to_ppm('tifftopnm', filename, out_filename)
+            elif is_gif(filename):
+                out_filename = output_pnm_filename(directory, file_, 'gif', 'ppm')
+                convert_to_ppm('giftopnm', filename, out_filename)
+            elif is_ppm(filename):
+                pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', dest='directory', required=True, help='Directory of files to convert')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--to', dest='to_ppm', action='store_true', help='Convert files in directory to PPM')
+    group.add_argument('--from', dest='from_ppm', action='store_true', help='Convert ~~ files in directory to original')
+    args = parser.parse_args()
+    if args.to_ppm:
+        files_to_ppm(args.directory)
+    elif args.from_ppm:
+        files_to_original(args.directory)
+    else:
+        raise Exception('WTF')
+    # convert(args.directory)
---
+++ utils/process.py
@@ -0,0 +1,158 @@
+import argparse
+import os
+
+import numpy as np
+
+
+
+SAMPLES_PER_PIXEL = 3
+IMAGE_PATH = os.path.join('..', 'img')
+
+
+class Pixel(object):
+    def __init__(self, red, blue, green):
+        self._red = red
+        self._blue = blue
+        self._green = green
+
+    @property
+    def red(self):
+        return self._red
+
+    @property
+    def blue(self):
+        return self._blue
+
+    @property
+    def green(self):
+        return self._green
+
+    def multiply(self, scalar):
+        return Pixel(self._red * scalar, self._blue * scalar, self._green * scalar)
+
+    def add(self, pixel):
+        return Pixel(self._red + pixel.red, self._blue + pixel.blue, self._green + pixel.green)
+
+    def __repr__(self):
+        return 'Pixel(%0.3f, %0.3f, %0.3f)' % (self.red, self.blue, self.green)
+
+    def __str__(self):
+        return self.__repr__()
+
+
+def is_comment(line):
+    return line.strip().startswith('#')
+
+
+def is_empty(line):
+    return len(line.strip()) == 0
+
+
+def read_header(fh):
+    file_type = None
+    file_dimensions = None
+    file_max_val = None
+
+    for line in fh:
+        if not is_comment(line) and not is_empty(line):
+            if not file_type:
+                file_type = line.strip()
+                # print 'FILE TYPE %s' % file_type
+            elif not file_dimensions:
+                file_dimensions = tuple([int(val) for val in line.strip().split()])
+                # print 'FILE DIMENSIONS ARE %s' % [v for v in file_dimensions]
+            elif not file_max_val:
+                file_max_val = int(line)
+                # print 'FILE MAX VALUE IS %s' % file_max_val
+                break
+            else:
+                raise Exception("WTF")
+
+    return file_type, file_dimensions, file_max_val
+
+
+def convert_ppm_line(line, cols, max_value):
+    values = line.strip().split()
+    assert (len(values) == SAMPLES_PER_PIXEL * cols)
+    pixels = []
+
+    for i in range(cols):
+        int_vals = [int(val) for val in values[i * SAMPLES_PER_PIXEL:i * SAMPLES_PER_PIXEL + SAMPLES_PER_PIXEL]]
+        assert len(int_vals) == 3
+        r, g, b = [float(val) / float(max_value) for val in int_vals]
+        pixel = Pixel(r, g, b)
+        # print pixel
+        pixels.append(pixel)
+
+    return pixels
+
+
+def convert_ppm_raster(fh, max_val):
+    samples = list()
+    for line in fh:
+        values = line.strip().split()
+        for sample in values:
+            assert int(sample) <= max_val
+            samples.append(int(sample))
+            if len(samples) == SAMPLES_PER_PIXEL:
+                sample_doubles = [float(val) / float(max_val) for val in samples]
+                pixel = Pixel(sample_doubles[0], sample_doubles[1], sample_doubles[2])
+                samples = list()
+                yield pixel
+
+
+def convert_ppm(filename):
+    image = list()
+    with open(filename, 'rb') as infile:
+        file_type, dimensions, max_val = read_header(infile)
+        rows, cols = dimensions
+        row = list()
+        for i, pixel in enumerate(convert_ppm_raster(infile, max_val)):
+            if len(row) == cols:
+                image.append(row)
+                assert len(row) == cols
+                row = list()
+                # print 'Completed row %d' % len(image)
+            if pixel is not None:
+                row.append(pixel)
+        if row:
+            assert len(row) == cols
+            image.append(row)
+        print 'Rows in image: %d' % len(image)
+        assert len(image) == rows
+
+
+def convert_ppm_rgb(filename):
+    # print filename
+    with open(filename, 'rb') as infile:
+        file_type, dimensions, max_val = read_header(infile)
+        rows, cols = dimensions
+
+        image_red = np.zeros((rows, cols))
+        image_green = np.zeros((rows, cols))
+        image_blue = np.zeros((rows, cols))
+        for i, pixel in enumerate(convert_ppm_raster(infile, max_val)):
+            row_num = i / cols
+            col_num = i % cols
+            # print 'Updating Pixel at row %d, col %d' % (row_num, col_num)
+            image_red[row_num][col_num] = pixel.red
+            image_green[row_num][col_num] = pixel.green
+            image_blue[row_num][col_num] = pixel.blue
+
+    return image_red, image_green, image_blue
+
+
+def apply_filter(image_ppm, stencil_pgm, iterations):
+    red_array, green_array, blue_array = convert_ppm_rgb(image_ppm)
+    # apply stencil
+    # reverse normalize
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', dest='image_file', required=True)
+    parser.add_argument('-s', dest='stencil_file', required=True)
+    parser.add_argument('-it', dest='iterations', type=int, default=2)
+    args = parser.parse_args()
+    image_file = os.path.abspath(os.path.join(IMAGE_PATH, args.image_file))
+    stencil_file = os.path.abspath(os.path.join(IMAGE_PATH, args.stencil_file))
+    apply_filter(image_file, stencil_file, args.iterations)
